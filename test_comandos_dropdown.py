673bab633e2d21d98be2286488388678cb3d5663
Ana Clara Ribeiro <anaclaraor@outlook.com>
2025-04-27 21:28:26 -0300

    Remove bibliotecas não utilizadas

--- test_comandos_dropdown.py
+++ test_comandos_dropdown.py
@@ -1,9 +1,7 @@
 import time
 
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service as ChromeService
 from selenium.webdriver.support.select import Select
-from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 
 browser = webdriver.Chrome()
